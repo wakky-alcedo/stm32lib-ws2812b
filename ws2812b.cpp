6e2e626b4a81d20d32ad4fbdb84b237581b4be15
alcedo-taku <taku.tatewaki@gmail.com>
2024-02-11 19:02:41 +0900

    add: class file を追加

---
+++ ws2812b.cpp
@@ -0,0 +1,10 @@
+#include <ws2812b.hpp>
+
+namespace ws2812b {
+
+WS2812B::WS2812B() {
+    // TODO Auto-generated constructor stub
+    
+}
+
+} /* namespace ws2812b */
---
+++ ws2812b.hpp
@@ -0,0 +1,13 @@
+#ifndef WS2812B_HPP_
+#define WS2812B_HPP_
+
+namespace ws2812b {
+
+class WS2812B {
+public:
+    WS2812B();
+};
+
+} /* namespace ws2812b */
+
+#endif /* WS2812B_HPP_ */
